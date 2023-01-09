2e13761b4948a869c4df637e25dc0210a187f1d4
Administrator <darren.huang@kapeixi.com>
2023-01-09 20:04:22 +0800

    更新大文件上传依赖

--- desgin/src/main/java/com/coder/desgin/controller/FileController.java
+++ desgin/src/main/java/com/coder/desgin/controller/FileController.java
@@ -1,11 +1,20 @@
 package com.coder.desgin.controller;
 
+import com.coder.desgin.entity.TChunkInfo;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 
 /**
- *  大文件分片上传控制器
+ *  大文件分片上传类
  */
 @Controller
 public class FileController {
 
+    private static String FILENAME = "";
+    @Value("${YouZ.upload.file.path}")
+    private String decryptFilePath;
+    @Value("${YouZ.upload.file.temp}")
+    private String decryptFilePathTemp;
+
+    public String uploadChunk(TChunkInfo chunkInfo)
 }
